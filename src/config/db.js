de3e74c6eabb3448784fc7f5ed20d6a9e5a85345
bryanavalossistemas <bryanavalossistemas@gmail.com>
2024-10-28 06:06:39 -0500

    modificacion en la config de db

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,17 @@
 const { Sequelize } = require("sequelize");
 const colors = require("colors");
 
-const database = process.env.DB_NAME;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT;
-const dialect = process.env.DB_DIALECT;
-
-const sequelize = new Sequelize(database, username, password, {
-  host: host,
-  port: port,
-  dialect: dialect,
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 const connectDB = async () => {
   try {
